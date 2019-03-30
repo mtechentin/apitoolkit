0bcdbeff7dac5b924ceae18098b10bf1e3a05a7e
Mike Techentin <miketechentin@gmail.com>
2019-03-30 01:44:45 -0600

    fixed exception handling logic errors refactored get_access_token control flow

--- venv/authtools/authtools.py
+++ venv/authtools/authtools.py
@@ -6,7 +6,7 @@ def get_access_token(**kwargs):
     '''
     Retrieve stored token, refresh if expired, store and output updated token
 
-    Args:
+    Kwargs:
         client_id: str
         client_secret: str
         token_url: str
@@ -16,25 +16,41 @@ def get_access_token(**kwargs):
     Returns:
         token: str
             Access Token
+
+    Notes:
+        The 'Bearer ' prefix is omitted from the Access Token and must be
+            added post-function-call
+        No support for refresh tokens yet.  Stay tuned.
+        Error Handling needs work.  I don't think print statements work in Power BI
+            without an open console.  Those custom exceptions are really intended to
+            get built out to handle some failure modes, tbd.
     '''
     client_id = kwargs['client_id']
     client_secret = kwargs['client_secret']
     token_url = kwargs['token_url']
     protected_url = kwargs['token_url']
 
-    # Retrieve and test old token.  Refresh if non-existent or expired and save updated token.
+    # Retrieve and test old token.  Return if valid.
     try:
         token = retrieve_token()
         client = OAuth2Session(client_id, token=token)
         probe = client.get(protected_url)
     except TokenExpiredError as error:
+        print(e)
+    except RetrieveError as error:
+        print(e)
+    finally:
+        return token['token']
+    # Generate and store new token
+    try:
         token = new_token(token_url, client_id, client_secret)
         save_token(token)
-    except ValueError as error:
-        token = new_token(token_url, client_id, client_secret)
-        save_token(token)
-
-    return token['token']
+    except TokenError as e:
+        print('Token Error: ' + e)
+    except SaveError as e:
+        print(e)
+    finally:
+        return token['token']
 
 
 def save_token(token):
@@ -43,13 +59,15 @@ def save_token(token):
 
     Args:
         token: dict
+    Yields:
+        file token.py, containing token dict
     '''
     try:
         file = open('token.py', 'w')
         file.write(token)
         file.close()
-    except SaveError as error:
-        print(error.message)
+    except:
+        raise SaveError
 
 
 def retrieve_token():
@@ -65,7 +83,7 @@ def retrieve_token():
         file.close()
         return token
     except:
-        raise ValueError
+        raise RetrieveError
 
 
 def new_token(token_url, client_id, client_secret):
@@ -85,19 +103,31 @@ def new_token(token_url, client_id, client_secret):
         oauth = OAuth2Session(client=client)
         token = oauth.fetch_token(token_url=token_url, client_id=client_id,
                                   client_secret=client_secret)
-    except TokenError as error:
-        print(error.message)
+        save_token(token)
+    except SaveError as e:
+        raise TokenError(e)
 
     return token
 
 
-class SaveError(Exception):
-    '''Token save exception stub'''
+class Error(Exception):
+    '''Base class for Custom Exceptions'''
+    pass
+
+
+class RetrieveError(Error):
+    '''Raised when token retrieval fails; triggers new token request'''
+    def __init__(self, message):
+        self.value = 'Error. Unable to retrieve token'
+
+
+class SaveError(Error):
+    '''Raised on Save Failure'''
     def __init__(self, message):
         self.message = 'Error. Unable to save token.'
 
 
-class TokenError(Exception):
-    '''Token generation exception stub'''
+class TokenError(Error):
+    '''Raised on failure to generate new token'''
     def __init__(self, message):
-        self.message = 'Error. Unable to generate token'
+        self.message = message
